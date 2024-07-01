e30bfee82ee0fc1c317f2ad2619f594315c2ceb9
Ulyanov-VN <ul.vn@ya.ru>
2024-07-01 20:24:34 +0300

    Проектная работа №1

---
+++ src/main/java/Car.java
@@ -0,0 +1,12 @@
+public class Car {
+    String name;
+    int speed;
+
+    public Car(
+        String name,
+        int speed
+    ) {
+        this.name = name;
+        this.speed = speed;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         // ваш код начнется здесь
         // вы не должны ограничиваться только классом Main и можете создавать свои классы по необходимости
-        System.out.println("Привет Мир");
+        Scanner scanner = new Scanner(System.in);
+        List<Car> cars = new ArrayList<>();
+
+        System.out.println("Введите 3 машины:");
+        for (int i = 1; i < 4; i++) {
+            System.out.println("Имя машины №" + i + ": ");
+            String name = scanner.next();
+            boolean flag = false;
+            int speed = -10;
+            while (!flag) {
+                System.out.println("Скорость машины №" + i + ": ");
+                speed = scanner.nextInt();
+                if (speed < 0 || speed > 250) {
+                    System.out.println("Скорость машины должна быть в интервале от 0 до 250.") ;
+                    flag = false;
+                } else
+                    flag = true;
+            }
+
+            cars.add(new Car(name, speed));
+        }
+
+        scanner.close();
+
+        Race race = new Race();
+        for (Car car : cars) {
+            race.newLeader(car);
+        }
+        System.out.println("Самая быстрая машина: " + race.leader);
     }
 }
